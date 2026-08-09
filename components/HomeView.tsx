ad52ff4ecd2227168e2aa182aa3d60496c9ea6b3
Aaditya Revandkar <aadityarevandkar@gmail.com>
2026-08-10 00:05:27 +0530

    fixed overlay

--- components/HomeView.tsx
+++ components/HomeView.tsx
@@ -112,7 +112,7 @@ export default function HomeView({ setActiveTab, onRequestQuote, settings }: Hom
             />
           ))}
         </div>
-        <div className="absolute inset-0 bg-gradient-to-t from-stone-950 via-black/30 to-transparent z-10" />
+        {/* <div className="absolute inset-0 bg-gradient-to-t from-stone-950 via-black/30 to-transparent z-10" /> */}
         <div className="absolute top-1/4 left-1/4 w-96 h-96 bg-[#785919]/10 dark:bg-[#eac076]/10 rounded-full blur-[120px] pointer-events-none" />
         <div className="absolute bottom-1/4 right-1/4 w-96 h-96 bg-blue-900/10 rounded-full blur-[120px] pointer-events-none" />
         <div className="absolute inset-0 engineering-grid opacity-[0.04] pointer-events-none z-10" />
